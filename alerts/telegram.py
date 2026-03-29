9a43ade49a93c2178e579759f4f0d7c7ea0c058d
jrojo0153-ux <jrojo0153@gmail.com>
2026-03-29 00:21:19 -0600

    Update telegram.py

--- alerts/telegram.py
+++ alerts/telegram.py
@@ -1,21 +1,20 @@
 import requests
 import os
 
-def enviar_alerta(mensaje: str):
+
+def enviar_alerta(mensaje):
     token = os.getenv("TELEGRAM_BOT_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
 
     if not token or not chat_id:
-        print("⚠️ Telegram no configurado correctamente")
+        print("⚠️ Telegram no configurado")
         return
 
     url = f"https://api.telegram.org/bot{token}/sendMessage"
 
-    try:
-        requests.post(url, data={
-            "chat_id": chat_id,
-            "text": mensaje
-        })
-        print("📩 Alerta enviada a Telegram")
-    except Exception as e:
-        print(f"❌ Error enviando alerta: {e}")
+    requests.post(url, data={
+        "chat_id": chat_id,
+        "text": mensaje
+    })
+
+    print("📲 Alerta enviada a Telegram")
