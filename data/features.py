de49d1577b77e9a73986dfadeb24ac2f999aed6a
jrojo0153-ux <jrojo0153@gmail.com>
2026-03-30 20:17:02 -0600

    Update features.py

--- data/features.py
+++ data/features.py
@@ -1,20 +1,20 @@
+# data/features.py
+
 import numpy as np
 
 def construir_features(partido):
-    try:
-        home_odds = partido["home_odds"]
-        away_odds = partido["away_odds"]
+    home_odds = float(partido["home_odds"])
+    away_odds = float(partido["away_odds"])
 
-        prob_home = 1 / home_odds
-        prob_away = 1 / away_odds
+    prob_home = 1 / home_odds
+    prob_away = 1 / away_odds
 
-        diff_prob = prob_home - prob_away
+    diff_prob = prob_home - prob_away
 
-        return np.array([
-            prob_home,
-            prob_away,
-            diff_prob
-        ])
-
-    except:
-        return np.array([0.5, 0.5, 0.0])
+    return np.array([
+        home_odds,
+        away_odds,
+        prob_home,
+        prob_away,
+        diff_prob
+    ])
