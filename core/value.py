d31877de2c16e4d020ed22d3c39cde3e92b3f290
jrojo0153-ux <jrojo0153@gmail.com>
2026-03-30 20:17:27 -0600

    Update value.py

--- core/value.py
+++ core/value.py
@@ -1,7 +1,17 @@
-from engine.ev_kelly import calcular_ev, calcular_kelly
+# core/value.py
 
-def evaluar_apuesta(prob, cuota):
-    ev = calcular_ev(prob, cuota)
-    kelly = calcular_kelly(prob, cuota)
+def evaluar_apuesta(prob, odds):
+    """
+    Calcula:
+    - EV (Expected Value)
+    - Kelly Criterion
+    """
+
+    if odds <= 1:
+        return -1, -1
+
+    ev = (prob * odds) - 1
+
+    kelly = ((prob * (odds - 1)) - (1 - prob)) / (odds - 1)
 
     return ev, kelly
